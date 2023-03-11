493e0448b995db9e521baa4dae51fa6a1e087922
fungoindia <83899086+fungoindia@users.noreply.github.com>
2023-03-11 17:21:47 +0800

    Update main.py

--- chatGPTEx/main.py
+++ chatGPTEx/main.py
@@ -139,6 +139,6 @@ if __name__ == "__main__":
     app.config['JSON_AS_ASCII'] = False
     app.config['DEBUG'] = True
     #local config uncomment this line
-    app.run(host="127.0.0.1",port=1234)
+#     app.run(host="127.0.0.1",port=1234)
     #docker config uncomment this line
-    # app.run(host="0.0.0.0", port = 5000)
+    app.run(host="0.0.0.0", port = 5000)
